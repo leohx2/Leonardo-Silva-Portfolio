f7ad5fb15046878cff9a60f7727f0bd3ad055a71
leohx2 <leobgse@gmail.com>
2025-09-11 18:43:33 +0100

    Creating a badge for each skill with neon effect and changing the website icon

--- Website/Website/src/components/Badges.tsx
+++ Website/Website/src/components/Badges.tsx
@@ -1,11 +1,31 @@
+import "./Badges.css";
 interface BadgesProps {
-  skill: "html" | "js" | "ts" | "css" | "tailwind" | "c";
+  skill:
+    | "html"
+    | "js"
+    | "ts"
+    | "css"
+    | "tailwind"
+    | "c"
+    | "react"
+    | "git"
+    | "preact";
+  text: string;
 }
 
-const Badges = () => {
+const Badges = ({ skill, text }: BadgesProps) => {
   return (
-    <div className="badge">
-      <div className="badge_inside"> content </div>
+    <div
+      className={`${skill}logo badge_container items-center flex justify-center rounded-md`}
+    >
+      <div className="badge_inside z-10 flex items-center justify-center rounded-md border-none bg-badge text-amber-50 flex-col gap-y-8">
+        <img
+          src={`../../public/images/${skill}logo.png`}
+          alt={`logo from ${skill}`}
+          className="h-20"
+        />
+        <p className="font-bold text-xl">{text}</p>
+      </div>
     </div>
   );
 };
